4ca1c725862851d4684f3f93ace87bf220d18331
PMuiruri <peninah.m.muiruri@gmail.com>
2018-05-12 16:49:09 +0300

    imported the contact and login components

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import Footer from './component/footer.js';
 import TaxNews from './component/taxNews.js';
 import HomePage from './component/home.js';
 import EditProfile from './component/editProfile.js';
+import Contact from './component/contact.js';
+import Login from './component/login.js';
+
 
 class App extends Component {
   render() {
     return (
       <Grid className="App" style={{fontFamily:'Questrial'}} fluid >
         <Header/>
-        <EditProfile/>
+        <Login />
         <Footer/>
       </Grid>
     );
