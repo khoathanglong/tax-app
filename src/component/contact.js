cffd2184d4a957e207b279347ee5fc5809ea1972
PMuiruri <peninah.m.muiruri@gmail.com>
2018-05-22 18:12:23 +0300

    fized some minor bugs

--- src/component/contact.js
+++ src/component/contact.js
@@ -8,19 +8,17 @@ export default class Contact extends Component {
       <div>
       <h2>Contact Us</h2>
       <h4>Feel free to drop us a line or two if you have further questions about our app or need some tax advice.</h4>
-      <form class="form-style">
+      <form class="form-contact-style">
         <ul>
           <li>
             <input type="text" name="name" class="field-style field-split align-left" placeholder="Name" required/>
             <input type="text" name="phone" class="field-style field-split align-left" placeholder="Phone" />
-
           </li>
           <li>
-            <input type="email" name="email" class="field-style field-full align-none" placeholder="Email" required/>
-
+            <input type="email" name="email" class="field-style field-full" placeholder="Email" required/>
           </li>
           <li>
-            <input type="text" name="subject" class="field-style field-full align-none" placeholder="Subject" required />
+            <input type="text" name="subject" class="field-style field-full" placeholder="Subject" required />
           </li>
           <li>
             <textarea name="message" class="field-style" placeholder="Message" required></textarea>
